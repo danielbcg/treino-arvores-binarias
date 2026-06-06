e01a869e4883520a0fb825647f96a4b40ab5057d
DanielBonypucminas <danielbony2105@gmail.com>
2026-06-06 16:53:28 -0300

    metodo remover feito por mim

--- ABB.java
+++ ABB.java
@@ -46,8 +46,67 @@ public class ABB<K, V> implements IMapeamento<K, V> {
     }
 
     @Override
-    public V remover(K chave) {
-        // Sua implementação original de remoção vai aqui
-        return null;
+    public void remover(K chave) throws Exception {
+        
+        raiz = remover(chave, raiz);
+
     }
+
+
+    private No<K,V> remover( K chave, No<K,V> i) throws Exception{
+
+        if(i==null){
+            throw new Exception("ERRO");
+        }
+
+        int comp = comparador.compare(chave, i.getChave());
+
+        if(comp>0){ //chave maior que elemento atual
+
+            i.setDir(remover(chave, i.getDir()));
+
+        }
+
+        else if(comp<0){ //chave menor que o elmento atual
+
+            i.setEsq(remover(chave, i.getEsq()));
+
+        }
+
+        else if(i.getDir()==null){ //caso so tenha o filho da esquerda - CASO DE 1 FILHO
+            i=i.getEsq();
+        }
+
+        else if(i.getEsq()==null){ //caso so tenha o filho da direita - CASO 1 FILHO
+            i=i.getDir();
+        }
+
+        else{ //caso tenha o filho da esquerda e da direita - CASO 2 FILHOS
+
+            i.setEsq(maiorEsq(i, i.getEsq()));
+            
+        }
+
+        return i;
+
+    }
+
+    private No<K,V> maiorEsq(No<K,V> i, No<K,V> j){
+
+        if(j.getDir()==null){
+            i.setChave(j.getChave()); //i copia o valor de j
+            i.setItem(j.getItem()); //TALVEZ NAO SEJA OBRIGATORIO ESSA LINHA OLHA DPS
+            j=j.getEsq(); // j avança pra esquerda p n ter o mesmo valor de i
+        }
+        else{
+            j.setDir(maiorEsq(i, j.getDir()));
+        }
+
+        return j;
+
+    }
+
+
+
+
 }
