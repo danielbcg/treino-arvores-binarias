11d3465f19700e531243c716b736644e4ba628fc
DanielBonypucminas <danielbony2105@gmail.com>
2026-06-18 14:10:47 -0300

    MAIS ESTUDOS JESUS CRISTO EU TE AMO

--- ABB.java
+++ ABB.java
@@ -507,6 +507,511 @@ public class ABB<K, V> implements IMapeamento<K, V> {
 
 
 
+    //dps apaga se quiser
+
+    public ABB<K,V> clonar(){
+        ABB<K,V> cloneDaArvore = new ABB<>();
+        cloneDaArvore.raiz = this.raiz.clonarNo();
+        return cloneDaArvore;
+    }
+
+
+
+
+
+    public ABB<K,V> recorte( K min, K max){
+
+        return recorte(raiz, min, max);
+
+    }
+
+    private ABB<K,V> recorte(No<K,V> no, K min, K max){
+
+        if(no==null){
+            return null;
+        }
+
+        int compMin = this.comparador.compare(min, no.getChave());
+        int compMax = this.comparador.compare(max, no.getChave());
+
+        if(compMin>0){
+            return recorte(no.getDir(), min, max);
+        }
+        else if(compMax<0){
+            return recorte(no.getEsq(), min, max);
+        }
+        else{
+
+            ABB<K,V> subArvore = recorte(no.getEsq(), min, max);
+            subArvore.inserir(no.getChave(), no.getItem());
+
+            ABB<K,V> subDir = recorte(no.getDir(),min, max);
+
+            subArvore.juntar(subDir);
+
+
+
+            return subArvore;
+        }
+
+    }
+
+    public ABB<K,V> juntar(ABB<K,V> outra){
+
+        return juntar(outra.raiz, outra);
+
+    }
+    private ABB<K,V> juntar(No<K,V> no, ABB<K,V> outra){
+
+        if(no==null){
+            return null;
+        }
+
+        this.inserir(no.getChave(), no.getItem());
+
+        juntar(no.getEsq(),outra);
+        juntar(no.getDir(), outra);
+
+        return this;
+
+    }
+
+
+
+
+
+
+
+    public Lista<K> chavesMaiores(K chave){
+        return chavesMaiores(raiz, chave);
+    }
+
+    private Lista<K> chavesMaiores(No<K,V> no, K chave){
+
+        if(no==null){
+            return new Lista<>();
+        }
+
+        int comp = this.comparador.compare(chave, no.getChave());
+
+        if(comp>=0){
+            return chavesMaiores(no.getDir(), chave);
+        }
+        else{
+
+            Lista<K> lista = chavesMaiores(no.getEsq(), chave);
+
+            lista.inserirFim(no.getChave());
+
+            Lista<K> dir = chavesMaiores(no.getDir(), chave);
+
+            lista.concatenar(dir);
+
+
+            return lista;
+            
+
+        }
+    }
+
+    //questao do claude
+
+        public Lista<V> valoresPorNivel(int nivel){
+
+            return valoresPorNivel(raiz, 0, nivel);
+
+        }
+        private Lista<V> valoresPorNivel(No<K,V> no, int nivelAtual, int nivelBuscado){
+
+            if(no==null){
+                return new Lista<>();
+            }
+
+            if(nivelAtual == nivelBuscado){
+                Lista<V> lista = new Lista<>();
+                lista.inserirFim(no.getItem());
+                return lista;
+            }
+            else{
+
+                Lista<V> esq = valoresPorNivel(no.getEsq(), nivelAtual+1, nivelBuscado);
+                Lista<V> dir = valoresPorNivel(no.getDir(), nivelAtual+1, nivelBuscado);
+
+                esq.concatenar(dir);
+                return esq;
+
+            }
+
+            
+
+        }
+
+
+
+
+        public Lista<K> chavesDasFolhas(){
+
+            if(this.raiz==null){
+                return new Lista<>();
+            }
+
+            return chavesDasFolhas(raiz);
+
+        }
+
+        private Lista<K> chavesDasFolhas(No<K,V> no){
+
+        
+            if(no==null){
+                return new Lista<>();
+            }
+
+            if(no.getEsq()==null && no.getDir()==null){
+
+                Lista<K> lista = new Lista<>();
+                lista.inserirFim(no.getChave());
+                return lista;
+
+            }
+            else{
+                
+                Lista<K> esq = chavesDasFolhas(no.getEsq());
+                Lista<K> dir = chavesDasFolhas(no.getDir());
+
+
+                esq.concatenar(dir);
+                return esq;
+
+            }
+
+
+
+        }
+
+
+
+        public Lista<No<K,V>> way (K chave){
+
+            return way(raiz, chave);
+
+        }
+
+        private Lista<No<K,V>> way (No<K,V> no, K chave){
+
+            if(no==null){
+                return new Lista<>();
+            }
+
+            int comp = this.comparador.compare(chave, no.getChave());
+
+            if(comp>0){
+               Lista<No<K,V>> lista = way(no.getDir(), chave);
+               lista.inserirInicio(no);
+               return lista;
+            }
+            else if(comp<0){
+                Lista<No<K,V>> lista = way(no.getEsq(), chave);
+                lista.inserirInicio(no);
+                return lista;
+            }
+            else{
+                Lista<No<K,V>> lista = new Lista<>();
+                lista.inserirInicio(no);
+                return lista;
+                
+            }
+            
+        }
+
+
+
+
+        public Lista<K> emOrdemCrescente(){
+            return emOrdemCrescente(raiz);
+        }
+
+        private Lista<K> emOrdemCrescente(No<K,V> no){
+
+            if(no==null){
+                return new Lista<>();
+            }
+
+            Lista<K> lista = emOrdemCrescente(no.getEsq());
+            lista.inserirFim(no.getChave());
+            Lista<K> dir = emOrdemCrescente(no.getDir());
+            lista.concatenar(dir);
+
+            return lista;
+
+        }
+
+
+        public Lista<K> emOrdemDecrescente(){
+            return emOrdemDecrescente(raiz);
+        }
+        private Lista<K> emOrdemDecrescente(No<K,V> no){
+
+
+            if(no==null){
+                return new Lista<>();
+            }
+
+            Lista<K> dir = emOrdemDecrescente(no.getDir());
+            dir.inserirFim(no.getChave());
+            Lista<K> esq = emOrdemDecrescente(no.getEsq());
+            dir.concatenar(esq);
+
+            return dir;
+
+        }
+
+
+        public int contarUmFilho(){
+            return contarUmFilho(raiz);
+        }
+
+        private int contarUmFilho(No<K,V> no){
+
+            if(no==null){
+                return 0;
+            }
+
+            if(no.getEsq()!=null && no.getDir()==null){
+                return 1+contarUmFilho(no.getEsq());
+            }
+            else if(no.getEsq()==null && no.getDir()!=null){
+                return 1+contarUmFilho(no.getDir());
+            }
+            else{
+                return contarUmFilho(no.getEsq())+contarUmFilho(no.getDir());
+            }
+
+        }
+
+
+
+
+
+
+
+        public int somaDosNiveis(){
+            return somaDosNiveis(raiz, 0);
+        }
+
+        private int somaDosNiveis(No<K,V> no, int nivelAtual){
+
+            if(no==null){
+                return 0;
+            }
+            
+            return nivelAtual+somaDosNiveis(no.getEsq(), nivelAtual+1)+somaDosNiveis(no.getDir(), nivelAtual+1);
+        }
+
+
+
+
+
+
+
+        public boolean eEstritamenteBinaria(){
+            return eEstritamenteBinaria(raiz);
+        }
+
+        private boolean eEstritamenteBinaria(No<K,V> no){
+
+            if(no==null){
+                return true;
+            }
+
+            if(no.getEsq()!=null && no.getDir()==null){
+                return false;
+            }
+            else if(no.getDir()==null && no.getEsq()!=null){
+                return false;
+            }
+            else{
+                return eEstritamenteBinaria(no.getEsq()) && eEstritamenteBinaria(no.getDir());
+            }
+
+        }
+
+
+
+
+
+        public K maiorChaveNoIntervalo(K min, K max){
+            return maiorChaveNoIntervalo(raiz, min, max);
+        }
+
+        private K maiorChaveNoIntervalo(No<K,V> no, K min, K max){
+
+            if(no==null){
+                return null;
+            }
+
+            int compMin = this.comparador.compare(min, no.getChave());
+            int compMax = this.comparador.compare(max, no.getChave());
+
+            if(compMin>0){
+                return maiorChaveNoIntervalo(no.getDir(), min, max);
+            }
+            else if(compMax<0){
+                return maiorChaveNoIntervalo(no.getEsq(), min, max);
+            }
+            else{
+                
+                
+
+
+            }
+
+
+
+        }
+
+
+
+        public K[] emOrdemVetor(){
+            return emOrdemVetor(raiz, 0, 0);
+        }
+
+        private void emOrdemVetor(No<K,V> no, K[] vetor, int[] indice){
+
+            if(no==null){
+                return;
+            }
+
+            emOrdemVetor(no.getEsq(), vetor, indice);
+            vetor[indice[0]]=no.getChave();
+            indice[0]++;
+            emOrdemVetor(no.getDir(), vetor, indice);
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+        public K[] emOrdemVetorInt(){
+            K[] vetor = (K[]) new Object[tamanho()];
+            emOrdemVetorInt(raiz, vetor, 0);
+            return vetor;
+        }
+
+        private int emOrdemVetorInt(No<K,V> no, K[] vetor, int indice){
+
+            if(no==null){
+                return indice;
+            }
+            
+
+            indice = emOrdemVetorInt(no.getEsq(), vetor, indice);
+            vetor[indice]=no.getChave();
+            indice++;
+            indice = emOrdemVetorInt(no.getDir(), vetor, indice);
+
+            return indice;
+
+
+        }
+
+
+
+
+
+        public K[] folhasVetor(){
+
+            K[] vetor = (K[]) new Object[tamanho()];
+            folhasVetor(raiz,vetor, 0);
+            return vetor;
+
+        }
+
+        private int folhasVetor(No<K,V> no, K[] vetor, int indice){
+
+            if(no==null){
+                return indice;
+            }
+
+            indice = folhasVetor(no.getEsq(), vetor, indice);
+            indice = folhasVetor(no.getDir(), vetor, indice);
+
+            if(no.getEsq()==null && no.getDir()==null){
+                vetor[indice]=no.getChave();
+                indice++;
+            }
+
+            return indice;
+
+
+
+        }
+
+
+
+
+        public K[] emOrdemDecrescenteVetor(){
+            K[] vetor = (K[]) new Object[tamanho()];
+            emOrdemDecrescenteVetor(raiz, vetor, 0);
+            return vetor;
+        }
+
+        private int emOrdemDecrescenteVetor(No<K,V> no, K[] vetor, int indice){
+
+
+            if(no==null){
+                return indice;
+            }
+
+            indice = emOrdemDecrescenteVetor(no.getDir(), vetor, indice);
+            vetor[indice]=no.getChave();
+            indice++;
+            indice = emOrdemDecrescenteVetor(no.getEsq(), vetor, indice);
+
+
+            return indice;
+
+
+
+        }
+
+
+
+        public K[] chavesPorNivelVetor(int nivel){
+            K[] vetor = (K[]) new Object[tamanho()];
+            chavesPorNivelVetor(raiz, vetor, 0, 0, nivel);
+            return vetor;
+        }
+
+        private int chavesPorNivelVetor(No<K,V> no, K[] vetor, int indice, int nivelAtual, int nivelBuscado){
+
+            if(no==null){
+                return indice;
+            }
+
+            indice=chavesPorNivelVetor(no.getEsq(), vetor, indice, nivelAtual+1, nivelBuscado);
+            if(nivelAtual==nivelBuscado){
+                vetor[indice]=no.getChave();
+                indice++;
+            }
+            indice=chavesPorNivelVetor(no.getDir(), vetor, indice, nivelAtual+1, nivelBuscado);
+
+            return indice;
+
+        }
+
+
+
+
+
+
+
 
 
 
---
+++ Lista.java
@@ -0,0 +1,98 @@
+public class Lista<E> {
+    private Celula<E> primeiro;
+    private Celula<E> ultimo;
+    private int tamanho;
+
+    public Lista() {
+        primeiro = new Celula<>();
+        ultimo = primeiro;
+        tamanho = 0;
+    }
+
+    public boolean vazia() {
+        return primeiro == ultimo;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void inserir(E novo, int posicao) {
+        if (posicao < 0 || posicao > tamanho) {
+            throw new IndexOutOfBoundsException("Posição inválida: " + posicao);
+        }
+
+        Celula<E> anterior = primeiro;
+        for (int i = 0; i < posicao; i++) {
+            anterior = anterior.getProximo();
+        }
+
+        Celula<E> novaCelula = new Celula<>(novo, anterior.getProximo());
+        anterior.setProximo(novaCelula);
+
+        if (anterior == ultimo) {
+            ultimo = novaCelula;
+        }
+
+        tamanho++;
+    }
+
+    public E remover(int posicao) {
+        if (vazia()) {
+            throw new RuntimeException("Lista vazia!");
+        }
+        if (posicao < 0 || posicao >= tamanho) {
+            throw new IndexOutOfBoundsException("Posição inválida: " + posicao);
+        }
+
+        Celula<E> anterior = primeiro;
+        for (int i = 0; i < posicao; i++) {
+            anterior = anterior.getProximo();
+        }
+
+        Celula<E> aRemover = anterior.getProximo();
+        anterior.setProximo(aRemover.getProximo());
+
+        if (aRemover == ultimo) {
+            ultimo = anterior;
+        }
+
+        tamanho--;
+        return aRemover.getItem();
+    }
+
+
+    public void inserirFim(E novo){
+
+        Celula<E> celulaNova = new Celula<>();
+
+        ultimo.setProximo(celulaNova);
+        ultimo=celulaNova;
+        tamanho++;
+
+    }
+
+    public void concatenar(Lista<E> outraLista){
+
+        Celula<E> aux = outraLista.primeiro.getProximo();
+
+        if(aux!=null){  
+            outraLista.inserirFim(aux.getItem());
+            aux=aux.getProximo();
+        }
+
+    }
+
+
+
+    public void inserirInicio(E novo){
+
+        Celula<E> celulaNova = new Celula<>(novo);
+
+        celulaNova.setProximo(primeiro.getProximo());
+        primeiro.setProximo(celulaNova);
+
+    }
+
+
+}
